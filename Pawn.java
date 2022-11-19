6e4fa3f642bcd5e6fb7eaaccb2cf4f103246c9ad
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-11-19 17:04:26 +0100

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,32 +1,15 @@
 import java.util.Objects;
 
 public class Pawn {
-	/* les pions des joueurs et le pions blanc */
-	private final String _color;
-	private int _pos;
+    private final String _color;
 
-	public Pawn(String color) {
-		Objects.requireNonNull(color, "Color missing");
-		
-		_color = color;
-		_pos = 0;
-	}
+    public Pawn(String color) {
+        Objects.requireNonNull(color);
+        _color = color;
+    }
 
-	public int pos() {
-		return _pos;
-	}
-	
-	public void move() {
-		_pos++;
-	}
-	
-	public String asciiPawn() {
-		return _color.substring(0, 1);
-	}
-
-	@Override
-	public String toString() {
-		return "color : " + _color + ", position :" + _pos;
-	}
-
+    @Override
+    public String toString() {
+        return _color.substring(0, 1);
+    }
 }

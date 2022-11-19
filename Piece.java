63147cc65cf0032fcef9288301c4a58a0477f8df
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-11-19 17:05:11 +0100

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,52 +1,78 @@
-import java.util.Objects;
 
-public record Piece(int[][] body, int cost, int nbMove, int nbButton) {
-	public Piece {
-		Objects.requireNonNull(body);
-		Objects.requireNonNull(cost);
-		Objects.requireNonNull(nbMove);
-		Objects.requireNonNull(nbButton);
+public class Piece {
+    private boolean[][] _body; // 1bis
+    private int _cost;
+    private int _buttons;
+    private int _moves;
+    private int xSize; 
+    private int ySize;
 
-		if (body.length > 9 || body[0].length > 9) {
-			throw new IllegalArgumentException("The piece can't be bigger than the quilt board");
-		}
+    public int getCost() {return _cost;}
+    public int getButtons() {return _buttons;}
+    public int getMoves() {return _moves;}
+    public int getXSize() {return xSize;}
+    public int getYSize() {return ySize;}
 
-		if (body.length != body[0].length) {
-			throw new IllegalArgumentException("The two-dimensional array 'body' must be square");
-		}
+    public boolean getBodyValue(int x, int y) {
+        return _body[y][x];
+    } 
+    
+    private void initPiece(String line) {
+    	char[] decomposition = line.toCharArray();
+    	int x, y;
+    	x = 0;
+    	y = 0;
+    	for (var elm : decomposition) {
+            switch (elm) {
+                case '1' -> {_body[x][y] = true; x++;}
+                case '0' -> {_body[x][y] = false; x++;}
+                case ',' -> {
+                	if (xSize != x) {
+                		throw new IllegalArgumentException("invalid piece");
+                	}
+                	y++;
+                	x = 0;
+                	}
+            }
+        }
+    }
+    
+    private void parseBody(String line) {
+    	var splitLine = line.split(",");
+    	ySize = splitLine.length;
+    	xSize = splitLine[0].length();
+    	_body = new boolean[xSize][ySize];
+    	initPiece(line);
+    }
 
-		if (cost < 0 || nbMove < 0 || nbButton < 0) {
-			throw new IllegalArgumentException("The piece must have a positive cost, number of moves and number of buttons");
-		}
+    public void parseLine(String line) {
+		var splitLine = line.split(":");
+
+	    parseBody(splitLine[0]);
+		_cost = Integer.parseInt(splitLine[1]);
+		_moves = Integer.parseInt(splitLine[2]);
+		_buttons = Integer.parseInt(splitLine[3]);
 	}
-	
-	public void displayPiece() {
-		System.out.println("Your piece :");
-		for (int i = 0; i < body.length; i++) {
-			for (int j = 0; j < body.length; j++) {
-				if (body[i][j] == 1) {
-					System.out.print('x');
-				} else {
-					System.out.print(' ');
-				}
-			}
-			System.out.println();
-		}
-	}
 
-	// Not useful for the moment but will maybe be
-	/*
-	 * private int findMax(int[][] lstPair, int x) { int max = body[1][x];
-	 * 
-	 * for (var i = 1; i < body.length; i++) { if (body[i][x] >= max) { max =
-	 * body[i][x]; } } return max + 1; }
-	 * 
-	 * public Piece rotateClockWise() { var newBody = new int[body.length][2]; var
-	 * newWidth = height; var newHeight = width;
-	 * 
-	 * for (var i = 0; i < newBody.length; i++) { newBody[i][0] = width - 1 -
-	 * body[i][1]; newBody[i][1] = body[i][0]; }
-	 * 
-	 * return new Piece(newBody, newHeight, newWidth, cost, nbMove, nbButton); }
-	 */
+    public boolean fitArea(int x, int y, int size) {
+        if (xSize + x >= size) {
+            return false;
+        }
+        if (ySize + y >= size) {
+            return false;
+        }
+        return true;
+    }
+    
+    public String bodyString() {
+    	var builder = new StringBuilder();
+        
+        for (int i = 0; i < ySize; i++) {
+        	for (int j = 0; j < xSize; j++) {
+        		builder.append(_body[j][i] ? "x" : " ");
+        	}
+        	builder.append("\n");
+        }
+        return builder.toString();
+    }
 }

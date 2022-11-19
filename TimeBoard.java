7e2392d91956db42d2569e4ff2fbe1d830012a3a
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-11-19 17:07:25 +0100

    Update TimeBoard.java

--- TimeBoard.java
+++ TimeBoard.java
@@ -1,35 +1,18 @@
+import java.util.ArrayList;
+import java.util.Objects;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class TimeBoard {
-	private final ArrayList<Box> board;
+    private final ArrayList<Box> _board = new ArrayList<Box>();
 
-	public TimeBoard() {
-		board = new ArrayList<Box>();
-	}
-
-	public ArrayList<Box> board() {
-		return board;
-	}
-
-	private void add(Box box) {
+    private void add(Box box) {
 		Objects.requireNonNull(box);
-		board.add(box);
+		_board.add(box);
 	}
 
-	public void initPlayerPawns(List<Player> players) {
-		Objects.requireNonNull(players);
-
-		for (int i = players.size() - 1; i >= 0; i--) {
-			board.get(0).add(players.get(i));
-		}
-	}
-
-	private void parseLine(String line) {
+    private void parseLine(String line) {
 		var boxesStatus = line.split(",");
 
 		for (var boxStatus : boxesStatus) {
@@ -37,35 +20,24 @@ public class TimeBoard {
 		}
 	}
 
-	public void loadTimeBoard(Path path) throws IOException {
+    public void loadTimeBoard(Path path) throws IOException {
 		try (var reader = Files.newBufferedReader(path)) {
 			String line;
-			String[] boxesStatus;
 			while ((line = reader.readLine()) != null) {
 				parseLine(line);
 			}
 		}
 	}
-	
-	public void movePawn(Player player) {
-		board.get(player.pawn().pos()).remove(player);
-		player.pawn().move();
-		board.get(player.pawn().pos()).add(player);
-	}
-	
-	public Player currentPlayer() {
-		Player currentPlayer = null;
-		var index = 0;
-		
-		while (currentPlayer == null) {
-			currentPlayer = board.get(index).getPlayer();
-			index++;
+
+	public void initPlayerPawns(Player[] players, int size) {
+		Objects.requireNonNull(players);
+
+		for (int i = size - 1; i >= 0; i--) {
+			_board.get(0).add(players[i]);
 		}
-		
-		return currentPlayer;
 	}
 
-	/* Graphic method - Ascii Version */
+    /* Graphic method - Ascii Version */
 	private void displayCaption() {
 		System.out.println("Caption :\n" + "  0 : button\n" + "  x : patch\n" + "  B, R : pawn of a player\n");
 	}
@@ -76,7 +48,7 @@ public class TimeBoard {
 
 	public void displayTimeBoard() {
 		displayCaption();
-		System.out.println(toString());
+		display();
 	}
 
 	private String printNTimes(String line, int n) {
@@ -88,25 +60,24 @@ public class TimeBoard {
 
 		return builder.toString();
 	}
-
-	@Override
-	public String toString() {
+	
+	public void display() {
 		var builder = new StringBuilder();
 
-		builder.append(printNTimes(" _____", board.size()));
-		builder.append(printNTimes("|     ", board.size()));
+		builder.append(printNTimes(" _____", _board.size()));
+		builder.append(printNTimes("|     ", _board.size()));
 
-		for (var box : board) {
-			var asciiPawn = " ";
+		for (var box : _board) {
+			String asciiPawn = " ";
 			if (box.getPlayer() != null) {
-				asciiPawn = box.getPlayer().pawn().asciiPawn();
+				asciiPawn = box.getPlayer().getPawn().toString();
 			}
-			builder.append(box.status()).append("  ").append(asciiPawn).append("  ");
+			builder.append(box.toString()).append("  ").append(asciiPawn).append("  ");
 		}
 		builder.append("\n");
-		builder.append(printNTimes("|_____", board.size()));
+		builder.append(printNTimes("|_____", _board.size()));
 
-		return builder.toString();
+		System.out.println(builder.toString());
 	}
 
 }

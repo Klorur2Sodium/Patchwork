d5248d3743a76a1916fde0f5e8eae6c97a6ea33c
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-11-25 18:06:26 +0100

    Update PieceHandler.java

--- PieceHandler.java
+++ PieceHandler.java
@@ -3,11 +3,13 @@ import java.util.Objects;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.lang.Math;
 
 public class PieceHandler {
 	private static final PieceHandler _instance = new PieceHandler();
 	
     private final ArrayList<Piece> _pieces;
+    private int _biggestPiece;
     private int _piecesDisplayed;
     private int _posNeutralPawn;
 
@@ -15,6 +17,7 @@ public class PieceHandler {
         _pieces = new ArrayList<>();
         _piecesDisplayed = 10;
         _posNeutralPawn = 0;
+        _biggestPiece = 0;
     }
     
     public static PieceHandler Handler() { return _instance; }
@@ -25,15 +28,13 @@ public class PieceHandler {
     	return index < _pieces.size()
     		? index
     		: index - _pieces.size();
-    	
-//		if (index >= _pieces.size()) {
-//			return index - _pieces.size();
-//		}
-//		return index;
 	}
 
     public boolean add(Piece p) {
         Objects.requireNonNull(p);
+        if (_biggestPiece < p.getYSize()) {
+        	_biggestPiece = p.getYSize();
+        }
         return _pieces.add(p);
     }
 
@@ -42,13 +43,23 @@ public class PieceHandler {
         _pieces.remove(p);
     }
 
+    public void mix() {
+    	int changementNumber = 10;
+    	for (int i = 0; i < changementNumber; i++) {
+    		var index = Math.random() * _pieces.size();
+    		var elem = _pieces.get((int)index);
+    		_pieces.remove(elem);
+    		_pieces.add(elem);
+    	}
+    }
+    
     public void loadPieces(Path path) throws IOException {
 		try (var reader = Files.newBufferedReader(path)) {
 			String line;
 			while ((line = reader.readLine()) != null) {
                 var p = new Piece();
                 p.parseLine(line);
-				_pieces.add(p);
+				add(p);
 			}
 		}
 	}
@@ -66,8 +77,7 @@ public class PieceHandler {
     	var boardBuilder = new BoardBuilder();
     	boardBuilder.display();
     }
-
- 
+    
     private class BoardBuilder {
     	
     	private String spacesString(int nbSpaces) {
@@ -99,8 +109,15 @@ public class PieceHandler {
             return builder.toString();
     	}
     	
-    	private String bodyString(int index) {
-            return _pieces.get(index).bodyString() + "   ";
+    	private String bodyString() {
+    		var builder = new StringBuilder();
+    		for (int i = 0; i < _biggestPiece; i++) {
+    			for (int j = 0; j < _pieces.size(); j++) {
+    				builder.append(_pieces.get(j).bodyLine(i)).append("  ");
+        		}
+    			builder.append("\n");
+    		}
+            return builder.toString();
         }
     	
     	private String costString(int index) {
@@ -112,11 +129,10 @@ public class PieceHandler {
         }
 
         private String buttonString(int index) {
-            return "b:" + _pieces.get(index).getButtons() + spacesString(_pieces.get(index).getXSize());
+            return "b :" + _pieces.get(index).getButtons() + spacesString(_pieces.get(index).getXSize());
     	}
         
         private void displayPieces() {
-        	var body = new StringBuilder();
         	var cost = new StringBuilder();
         	var moves = new StringBuilder();
         	var button = new StringBuilder();
@@ -124,12 +140,11 @@ public class PieceHandler {
         	
         	for (int i = _posNeutralPawn; i < _posNeutralPawn + _piecesDisplayed && i < _pieces.size(); i++) {
         		index = getRealIndex(i);
-        		body.append(bodyString(index));
         		cost.append(costString(index));
         		moves.append(movesString(index));
     			button.append(buttonString(index));
     		}
-        	System.out.println(body.toString());
+        	System.out.println(bodyString());
         	System.out.println(cost.toString());
         	System.out.println(moves.toString());
         	System.out.println(button.toString());

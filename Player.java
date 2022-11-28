68f48b8cf42c3aac127eccbabbdb2b51baadb848
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-11-28 18:24:48 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -7,6 +7,7 @@ public class Player {
 	private final String _name;
 	private int _wage;
 	private Pawn _pawn;
+	private int _position;
 
     public Player(String name, String color) {
 		Objects.requireNonNull(name);
@@ -15,12 +16,14 @@ public class Player {
 		_wage = 0;
 		_buttonsCount = 5;
 		_pawn = new Pawn(color);
+		_position = 0;
 
 		_quiltBoard = new QuiltBoard();
 	}
 
 	public String getName() {return _name;}
 	public Pawn getPawn() {return _pawn;}
+	public int getPosition() {return _position;}
 
 	
     public boolean canByPiece(Piece piece) {
@@ -44,9 +47,21 @@ public class Player {
 
 	public void skipTurn(int nbMoves) {
 		if (nbMoves < 0) {
-			throw new IllegalArgumentException("the ayer can't move back");
+			throw new IllegalArgumentException("the player can't move back");
 		}
 		earnButtons(nbMoves);
+		_position += nbMoves;
+	}
+	
+	public boolean byPiece(Piece p, Scanner scan) {
+		if (!canByPiece(p)) {
+			return false;
+		}
+		placingPhaseDemo(p, scan);
+		_position += p.getMoves();
+		_buttonsCount -= p.getCost();
+		_wage += p.getButtons();
+		return true;
 	}
 
 	// Methods - Ascii version
@@ -72,8 +87,6 @@ public class Player {
 
 	public void placingPhaseDemo(Piece piece, Scanner scanner) {
 		String userInput;
-		_buttonsCount -= piece.getCost();
-		_wage += piece.getButtons();
 		
 		do {
 			System.out.println("Do you want to place your piece automaticly y/n");

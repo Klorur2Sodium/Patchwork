c856453fd1ce1f0db003382f5f3aab9c7a44f32d
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-11-28 18:22:53 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,34 +7,48 @@ public class Main {
     	var scanner = new Scanner(System.in);
         var timeBoard = new TimeBoard();
         var pieceHandler = PieceHandler.Handler();
-        int[] positions = {1, 1};
-        Player[] players = {new Player("player1", "Blue"), new Player("player2", "Red")};
+        // missing  : check if the names aren't the same
+        Player[] players = {new Player(getName(scanner, 1), "Blue"), new Player(getName(scanner, 2), "Red")};
         var current = 0;
         var other = 1;
         int playerChoice = 1;
 
         initTimeBoard("load_time_board_demo", timeBoard, scanner);
         initPieceHandler("load_pieces", pieceHandler, scanner);
-        timeBoard.initPlayerPawns(players, 2);
+//        timeBoard.initPlayerPawns(players, 2);
 
-        while (pieceHandler.getSize() > 0) {
-			timeBoard.displayTimeBoard();
-			pieceHandler.display();
-
+        game(timeBoard, pieceHandler); // prints the captions 
+        
+        while (pieceHandler.getSize() > 0) { // needs to be modify
 			playerChoice = players[current].buyingPhase(scanner);
 
 			if (playerChoice == 0) {
-				players[current].skipTurn(positions[current] - positions[other]);
-			} else if (players[current].canByPiece(pieceHandler.getPiece(playerChoice - 1))) {
-				var piece = pieceHandler.getPiece(playerChoice - 1);
-				players[current].placingPhaseDemo(piece, scanner);
-                positions[current] = piece.getMoves();
-				pieceHandler.remove(piece);
+				if (players[0].getPosition() != players[1].getPosition()) {
+					players[current].skipTurn(players[other].getPosition() - players[current].getPosition());
+				}
+			} else if (players[current].byPiece(pieceHandler.getPiece(playerChoice - 1), scanner)) {
+				pieceHandler.remove(pieceHandler.getPiece(playerChoice - 1));
 			}
-			current = currentPlayer(positions, current);
+			
+			current = currentPlayer(players, current);
             other = otherPlayer(current);
 			timeBoard.demarcateTurns();
+			
+			
+			timeBoard.displayTimeBoard(false);
+			pieceHandler.display(false);
         }
+    }
+    
+    private static void game(TimeBoard t, PieceHandler p) {
+    	t.displayTimeBoard(true);
+    	p.display(true);
+    	System.out.println("\nlet's go !!\n");
+    }
+    
+    private static String getName(Scanner scan, int number) {
+    	System.out.println("please enter player" + number + "'s name ");
+    	return scan.next();
     }
 
     private static void initTimeBoard(String file, TimeBoard t, Scanner scanner) {
@@ -51,7 +65,7 @@ public class Main {
     private static void initPieceHandler(String file, PieceHandler p, Scanner scanner) {
         try {
         	p.loadPieces(Path.of(file));
-		p.mix();
+        	p.mix();
         } catch (IOException e) {
         	System.err.println(e.getMessage());
         	System.exit(1);
@@ -60,11 +74,11 @@ public class Main {
         }
 	}
 
-    private static int currentPlayer(int[] playersPositions, int current) {
-        if (playersPositions[0] > playersPositions[1]) {
+    private static int currentPlayer(Player[] players, int current) {
+        if (players[0].getPosition() > players[1].getPosition()) {
             return 1;
         }
-        if (playersPositions[0] < playersPositions[1]) {
+        if (players[0].getPosition() < players[1].getPosition()) {
             return 0;
         }
         return current;
@@ -77,3 +91,4 @@ public class Main {
         return 0;
     }
 }
+

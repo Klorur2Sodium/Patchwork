a1edbcd921cd0bdcf5ca0a50455f0229f07ae665
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-12-04 19:19:05 +0100

    Update IGamePlayerSelector.java

--- IGamePlayerSelector.java
+++ IGamePlayerSelector.java
@@ -1,3 +1,14 @@
+/**
+ * 
+ * This interface expose a method 
+ * to set the players.
+ * Once the players are set the next step is to build a game
+ * Thus the addPlayer method returns the IGameBuilder Interface.
+ * 
+ * @author Cousson Sophie
+ * @author Fraize Victor
+ *
+ */
 
 public interface IGamePlayerSelector {
 	IGameBuilder addPlayers();

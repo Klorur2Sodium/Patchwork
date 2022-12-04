0e5289ce05bf4a980ec2fb80ccde8ca1f2d78166
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-12-04 19:18:44 +0100

    Update IGameBuilder.java

--- IGameBuilder.java
+++ IGameBuilder.java
@@ -1,3 +1,12 @@
+/**
+ * 
+ * This interface expose a method 
+ * to build a game.
+ * 
+ * @author Cousson Sophie
+ * @author Fraize Victor
+ *
+ */
 
 public interface IGameBuilder {
 	Game build();

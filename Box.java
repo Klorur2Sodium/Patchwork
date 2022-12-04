0502082fe7833606676be4c7ff6fefe9ee5c1159
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-12-04 21:16:55 +0100

    Update Box.java

--- Box.java
+++ Box.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * This class stores the information about a box, it's type (_status) 
+ * and the players on it 
+ * 
+ * @author FRAIZE Victor
+ * @author COUSSON Sophie
+ *
+ */
+
 public class Box {
 	private char _status;
 	private ArrayList<Player> _players = new ArrayList<>();
@@ -18,24 +28,51 @@ public class Box {
 		}
 	}
 	
+	/**
+	 * The method returns the players List
+	 * 
+	 * @return a List of Players
+	 */
 	public List<Player> getPlayers() {
 		return _players;
 	}
 	
+	/**
+	 * 
+	 * The method returns a char that represent the type of the Box
+	 * | or 0 or x (nothing, a button, a patch)
+	 * 
+	 * @return a char 
+	 */
 	public char getStatus() {
 		return _status;	
 	}
 
+	/**
+	 * The method adds a player to the List of Players
+	 * 
+	 * @param player to add
+	 */
 	public void add(Player player) {
 		Objects.requireNonNull(player);
 		_players.add(player);
 	}
 
+	/**
+	 * The method removes a player form the List of players
+	 * 
+	 * @param player to remove
+	 */
 	public void remove(Player player) {
 		Objects.requireNonNull(player);
 		_players.remove(player);
 	}
 
+	/**
+	 * The method returns the last player added to the List of players
+	 * 
+	 * @return a Player
+	 */
 	public Player getPlayer() {
 		int playersSize = _players.size();
 
@@ -45,6 +82,13 @@ public class Box {
 		return null;
 	}
 
+	/**
+	 * The method launch events that match to the _status of the box.
+	 * 
+	 * @param player that plays
+	 * @param scanner
+	 * @param version the version of the game
+	 */
 	public void boxEvent(Player player, Scanner scanner, String version) {
 		Objects.requireNonNull(player);
 

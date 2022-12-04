cf79a221d29630a9e02493660260f51f5068ec49
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-12-04 19:19:25 +0100

    Update IGameVersionSelector.java

--- IGameVersionSelector.java
+++ IGameVersionSelector.java
@@ -1,3 +1,14 @@
+/**
+ * 
+ * This interface expose a method 
+ * to choose a game version.
+ * Once the version is set. The next step is to select the players
+ * Thus the chooseVersion returns the interface exposing the player selection/setting
+ * 
+ * @author Cousson Sophie
+ * @author Fraize Victor
+ *
+ */
 
 public interface IGameVersionSelector {
 	IGamePlayerSelector chooseVersion();
